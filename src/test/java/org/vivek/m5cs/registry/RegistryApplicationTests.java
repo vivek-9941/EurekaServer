ca79fc106a7ecdba214780af8bcd8ec94d62d3d1
Lenovo <vivekmane9860@gmail.com>
2025-09-06 18:13:27 +0530

    initial commit

---
+++ src/test/java/org/vivek/m5cs/registry/RegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vivek.m5cs.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
